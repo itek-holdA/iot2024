85e3a93ff2d0b6dcf9e72c5e92bebb4fddd0ee9c
migr-kea <migr0002@stud.kea.dk>
2024-12-11 00:30:11 +0100

    Add files via upload
    Changes:
    
    - Tilføjet WiFi AP

--- boot.py
+++ boot.py
@@ -1,14 +1,28 @@
-import urequests as requests
 import network
-import time
-import gc
-import secret
+from time import sleep
 
-def Connect_Wifi(ssid, password):
-    station = network.WLAN(network.STA_IF)
-    station.active(True)
-    station.connect(ssid, password)
-    while station.isconnected() == False:
-        pass
-    print('Connection successful')
-    print(station.ifconfig())
+# Opret Wi-Fi-station og forbind til et netværk
+sta = network.WLAN(network.STA_IF)
+sta.active(True)
+
+# Kontroller, om stationen er aktiv
+if not sta.isconnected():
+    print("Trying to connect...")
+    sta.connect("", "")
+
+    # Vent på forbindelse
+    while not sta.isconnected():
+        print("Connecting")
+        sleep(0.5)
+
+    if sta.isconnected():
+        print("STA connected:", sta.ifconfig())
+    else:
+        print("Connection failed")
+
+# Opret Access Point (Hotspot)
+ap = network.WLAN(network.AP_IF)
+ap.active(True)
+ap.config(essid="ESP32_Hotspot", password="12345678")
+
+print("AP active:", ap.ifconfig())
